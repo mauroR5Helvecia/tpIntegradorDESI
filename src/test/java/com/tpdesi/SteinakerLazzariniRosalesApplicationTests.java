e3ba196df27cd6f949ce9ebb55969e771a0692d9
mauroR5Helvecia <rosalesmauro369@gmail.com>
2025-05-27 16:25:03 -0300

    agrego proyecto springboot con dependencias spring web, pring data jpa, driver mysql y configuracion en aplications.properties para conectarse a la base de datos

---
+++ src/test/java/com/tpdesi/SteinakerLazzariniRosalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpdesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteinakerLazzariniRosalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
